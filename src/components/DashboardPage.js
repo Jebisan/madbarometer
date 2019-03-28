204ac8afb4b2505a8180924f5c59e7628d91371a
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2019-03-28 21:24:55 +0100

    Added calculations

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -7,56 +7,88 @@ export class DashboardPage extends React.Component {
   constructor() {
     super();
     this.state = {
+      restaurantName:'',
       quality:0,
       time:0,
       hygiene:0,
-      totalPoint: 0
+      totalPoint: 0,
+      temperatureColor: '',
+      calculated: false
     };
   }
 
-  setTotalPoint = (e) => {
+  calculateTotalPoints = (e) => {
     const totalPoint = Number(this.state.quality)+Number(this.state.time)+Number(this.state.hygiene);
     this.setState(() => ({ totalPoint}));
+    this.setState(() => ({ calculated:true}));
   }
 
+  onRestaurantChangeName = (e) => {
+    const restaurantName = e.target.value;
+    this.setState(() => ({
+      restaurantName
+          }));
+        };
+
   onTimeChange = (e) => {
-    const time = e.target.value;
+    const time = e.target.value/3;
     this.setState(() => ({
         time
           }));
         };
   
   onHygieneChange = (e) => {
-    const hygiene = e.target.value;
+    const hygiene = e.target.value/3;
     this.setState(() => ({
         hygiene
                 }));
               };
 
 onQualityChange = (e) => {
-  const quality = e.target.value;
+  const quality = e.target.value/3;
                 this.setState(() => ({
                   quality
                             }));
                           };
+/*
+setTemperaturColor = () => {
+  if( this.state.totalPoint>70){
+    this.setState(() => ({temperatureColor:'green'}));}
+    else if( this.state.totalPoint >=30 && this.state.totalPoint<=70)
+    this.setState(() => ({temperatureColor:'orange'}));
+    else if (this.state.totalPoint<30){
+      this.setState(() => ({temperatureColor:'red'}));
+    }
+    console.log(this.state.totalPoint, this.state.temperatureColor)
+  }
+*/
+getState = () => {
+  console.log(this.state);
+}
 
   render () {
     return (
       <div>
       <h1>MadBarometeret</h1>
-      <input  onChange={this.onTimeChange} type="number" placeholder='Time'     /> 
-      <input  onChange={this.onHygieneChange} type="number" placeholder='Hygiene'/>
-      <input  onChange={this.onQualityChange} type="number" placeholder='Quality'/>
-      <button onClick={this.setTotalPoint}>Calculate!</button>
-      <div className="center">
+      <input  onChange={this.onRestaurantChangeName} type="text" placeholder='Restaurant Name'/>
+      <br></br> 
+      <input  onChange={this.onTimeChange} type="number" placeholder='Time'/>%
+      <br></br> 
+      <input  onChange={this.onHygieneChange} type="number" placeholder='Hygiene'/>%
+      <br></br> 
+      <input  onChange={this.onQualityChange} type="number" placeholder='Quality'/>%
+      <br></br> 
+      <button onClick={this.calculateTotalPoints}>Calculate!</button>
+      <h2 className="textcenter">Total {this.state.calculated?this.state.totalPoint+ " points!":null} </h2>
       
+      <div className="center">
       <Thermometer 
           min={0}
           max={100}
-          width={40}
-          height={600}
+          width={20}
+          height={400}
           backgroundColor={'#BDC0BA'}
-          fillColor={'#D0104C'}
+          fillColor={this.state.totalPoint<=50?'red':'green'}
           current={this.state.totalPoint}
       />
     </div>	
