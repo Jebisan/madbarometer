1f5bd4ca20051d2b609bdf48b83e7af865f2837a
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2019-03-28 20:04:49 +0100

    Added input functionalities

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -2,21 +2,68 @@ import React from 'react';
 import Thermometer from "react-thermometer";
 
 
-const DashboardPage = () => (
-  <div>
-    MadBarometeret
-    <div className="center">
-		<Thermometer 
-				min={0}
-				max={100}
-				width={40}
-				height={600}
-				backgroundColor={'#BDC0BA'}
-				fillColor={'#D0104C'}
-				current={10}
-		/>
-	</div>	
-  </div>
-);
+export class DashboardPage extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      quality:0,
+      time:0,
+      hygiene:0,
+      totalPoint: 0
+    };
+  }
+
+  setTotalPoint = (e) => {
+    const totalPoint = Number(this.state.quality)+Number(this.state.time)+Number(this.state.hygiene);
+    this.setState(() => ({ totalPoint}));
+  }
+
+  onTimeChange = (e) => {
+    const time = e.target.value;
+    this.setState(() => ({
+        time
+          }));
+        };
+  
+  onHygieneChange = (e) => {
+    const hygiene = e.target.value;
+    this.setState(() => ({
+        hygiene
+                }));
+              };
+
+onQualityChange = (e) => {
+  const quality = e.target.value;
+                this.setState(() => ({
+                  quality
+                            }));
+                          };
+
+  render () {
+    return (
+      <div>
+      <h1>MadBarometeret</h1>
+      <input  onChange={this.onTimeChange} type="number" placeholder='Time'     /> 
+      <input  onChange={this.onHygieneChange} type="number" placeholder='Hygiene'/>
+      <input  onChange={this.onQualityChange} type="number" placeholder='Quality'/>
+      <button onClick={this.setTotalPoint}>Calculate!</button>
+      <div className="center">
+      
+      <Thermometer 
+          min={0}
+          max={100}
+          width={40}
+          height={600}
+          backgroundColor={'#BDC0BA'}
+          fillColor={'#D0104C'}
+          current={this.state.totalPoint}
+      />
+    </div>	
+    </div>
+    )
+  }
+ 
+
+}
 export default DashboardPage;
